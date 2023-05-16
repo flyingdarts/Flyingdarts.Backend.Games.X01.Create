3b10dfb1702ad44266c38553811a56c80ff71c0c
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-16 13:24:14 +0200

    Update create x01 game lambda

--- CQRS/CreateX01GameCommand.cs
+++ CQRS/CreateX01GameCommand.cs
@@ -1,7 +1,12 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using System;
+using Amazon.Lambda.APIGatewayEvents;
 using MediatR;
 
 public class CreateX01GameCommand : IRequest<APIGatewayProxyResponse>
 {
-
+    public int Sets { get; set; }
+    public int Legs { get; set; }
+    public int StartScore { get; set; }
+    public string RoomId { get; set; }
+    public Guid UserId { get; set; }
 }
--- CQRS/CreateX01GameCommandHandler.cs
+++ CQRS/CreateX01GameCommandHandler.cs
@@ -1,15 +1,40 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using System.Text.Json;
+using Amazon.Lambda.APIGatewayEvents;
 using System.Threading;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+using Flyingdarts.Persistence;
+using Flyingdarts.Shared;
 using MediatR;
+using Microsoft.Extensions.Options;
 
 public class CreateX01GameCommandHandler : IRequestHandler<CreateX01GameCommand, APIGatewayProxyResponse>
 {
-    public CreateX01GameCommandHandler()
+    private readonly IDynamoDBContext _dbContext;
+    private readonly IOptions<ApplicationOptions> _applicationOptions;
+
+    public CreateX01GameCommandHandler(IDynamoDBContext DbContext, IOptions<ApplicationOptions> ApplicationOptions)
     {
+        _dbContext = DbContext;
+        _applicationOptions = ApplicationOptions;
     }
     public async Task<APIGatewayProxyResponse> Handle(CreateX01GameCommand request, CancellationToken cancellationToken)
     {
-        return new APIGatewayProxyResponse { StatusCode = 200 };
+        var game = Game.Create(2, X01GameSettings.Create(1, 3), request.RoomId);
+        var gamePlayer = GamePlayer.Create(game.GameId, request.UserId);
+        
+        var gameWrite = _dbContext.CreateBatchWrite<Game>(_applicationOptions.Value.ToOperationConfig());
+        gameWrite.AddPutItem(game);
+
+        var gamePlayerWrite = _dbContext.CreateBatchWrite<GamePlayer>(_applicationOptions.Value.ToOperationConfig());
+        gamePlayerWrite.AddPutItem(gamePlayer);
+
+        await gameWrite.Combine(gamePlayerWrite).ExecuteAsync(cancellationToken);
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(new { Game = game, Players = new[]{ gamePlayer }})
+        };
     }
 }
